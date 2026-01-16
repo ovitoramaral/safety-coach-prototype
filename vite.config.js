ac32e782f1007066a337245f3d0291c02ae77220
Vitor <vitor.amaral@intercom.io>
2026-01-16 12:23:06 +0100

    Fix: Use index.html as main entry point for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index-app.html'
-      }
-    }
+    outDir: 'dist'
   },
   server: {
-    port: 3001,
-    open: true
+    port: 3001
   }
 });
